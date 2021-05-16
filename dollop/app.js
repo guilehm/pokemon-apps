3c35de00410277b5e3d4bc1c017173730401e660
guilherme <guile.hm@hotmail.com>
2021-05-16 17:53:07 -0300

    improve dollop app

--- dollop/app.js
+++ dollop/app.js
@@ -1,19 +1,22 @@
-const Express = require('express')
+const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const logger = require('./utils/logger')
-
 
 const PORT = process.env.PORT || 3000
 const DEBUG = process.env.DEBUG || 1
 
 
-const app = new Express()
+const app = new express()
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('short'))
 app.use(helmet())
 
 
-app.get('/', (req, res) => res.json({ ok: true }))
+app.post('/api/delay/', (req, res) => {
+  const { time } = req.body
+  setTimeout(() => res.json({ time: `${time}s` }), time * 1000)
+})
 
 app.listen(PORT, () => {
   const message = parseInt(DEBUG) ? 'Starting development server on port' : 'App listening on port'
