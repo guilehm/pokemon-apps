d77ba43d24486ff874096cd32424f6a77b997580
guilherme <guile.hm@hotmail.com>
2021-05-16 17:17:20 -0300

    update dollop app

--- dollop/app.js
+++ dollop/app.js
@@ -4,13 +4,13 @@ const helmet = require('helmet')
 const logger = require('./utils/logger')
 
 
-const app = new Express()
-
-app.use(morgan('short'))
-app.use(helmet())
-
 const PORT = process.env.PORT || 3000
 const DEBUG = process.env.DEBUG || 1
+
+
+const app = new Express()
+app.use(morgan('short'))
+app.use(helmet())
 
 
 app.get('/', (req, res) => res.json({ ok: true }))
