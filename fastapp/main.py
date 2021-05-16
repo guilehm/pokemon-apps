f47f0841106a477e4ac2b6aaec36c43016591958
guilherme <guile.hm@hotmail.com>
2021-05-16 17:53:00 -0300

    improve fastapp

--- fastapp/main.py
+++ fastapp/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+import asyncio
+
+import httpx
+from fastapi import FastAPI, HTTPException
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from random import randint
+
+
+DOLLOP_URL = 'http://dollop'
 
 app = FastAPI()
 
 
 @app.get('/')
-def index(request: Request):
-    return JSONResponse({
-      'success': True,
-    })
+async def index(request: Request):
+    async def make_post_request(url):
+        async with httpx.AsyncClient(timeout=30) as client:
+            response = await client.post(url, data={'time': randint(1, 3)})
+            try:
+                response.raise_for_status()
+            except (httpx.RequestError, httpx.HTTPStatusError):
+                raise HTTPException(status_code=400)
+        return response.json()
 
+    urls = (f'{DOLLOP_URL}/api/delay/' for _ in range(20))
+    results = await asyncio.gather(*[make_post_request(url) for url in urls])
+    return JSONResponse(results)
