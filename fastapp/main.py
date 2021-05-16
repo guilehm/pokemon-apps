21fa9c2a4a5e2f1b30fdb957e7fbc2504b6d4dd3
guilherme <guile.hm@hotmail.com>
2021-05-16 18:17:15 -0300

    update urls

--- fastapp/main.py
+++ fastapp/main.py
@@ -1,27 +1,27 @@
 import asyncio
-from random import randint
+from random import random, randint, uniform
 
 import httpx
 from fastapi import FastAPI, HTTPException
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
-DOLLOP_URL = 'http://dollop'
+API_URL = 'http://spoon-proxy'
 
 app = FastAPI()
 
 
 @app.get('/')
 async def index(request: Request):
-    async def make_post_request(url):
+    async def make_get_request(url):
         async with httpx.AsyncClient(timeout=30) as client:
-            response = await client.post(url, params={'time': randint(1, 3)})
+            response = await client.get(url, params={'time': f'{uniform(1, 3)}'})
             try:
                 response.raise_for_status()
             except (httpx.RequestError, httpx.HTTPStatusError):
                 raise HTTPException(status_code=400)
         return response.json()
 
-    urls = (f'{DOLLOP_URL}/api/delay/' for _ in range(20))
-    results = await asyncio.gather(*[make_post_request(url) for url in urls])
+    urls = (f'{API_URL}/api/delay/' for _ in range(20))
+    results = await asyncio.gather(*[make_get_request(url) for url in urls])
     return JSONResponse(results)
