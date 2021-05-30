e2fbbeb650c13fddcf6165f87ebcd51729a5c2e3
guilherme <guile.hm@hotmail.com>
2021-05-30 20:43:15 -0300

    fix locations, update index, refactor app

--- valtr/src/App.tsx
+++ valtr/src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+import ApiService from './services/api-service'
+import { PokemonListResult } from './services/types'
+
+const Api = new ApiService()
+
+
 const App = () => {
+
+  const [pokemonList, setPokemonList] = useState<PokemonListResult[] | []>([])
+
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      const response = await Api.getPokemonList()
+      setPokemonList(response.data.results)
+    }
+    fetchPokemonList()
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {pokemonList.map((result: PokemonListResult) =>
+        <a key={result.name} href={`${result.url}`}><h2>{result.name}</h2></a>
+      )}
+
     </div>
   )
 }
