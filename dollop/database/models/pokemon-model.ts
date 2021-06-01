a47502647f9b7e17ae91c0559421952dd17acaf6
guilherme <guile.hm@hotmail.com>
2021-06-01 17:09:05 -0300

    create pokemon schema

---
+++ dollop/database/models/pokemon-model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from 'mongoose'
+
+type PokemonSchemaType = {
+  id: number
+  name: string
+  order?: number
+  height?: number
+  weight?: number
+}
+
+const PokemonSchema: Schema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  order: {
+    type: Number,
+  },
+  height: {
+    type: Number,
+  },
+  weight: {
+    type: Number,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+}, { strict: false })
+
+const Pokemon = mongoose.model('Pokemon', PokemonSchema)
+
+
+export default Pokemon
+export { PokemonSchemaType }
