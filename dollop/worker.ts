2c2e1c421ee02bc3afb22ab0b651d6e9d0387461
guilherme <guile.hm@hotmail.com>
2021-06-01 14:27:09 -0300

    transform dollop in worker

---
+++ dollop/worker.ts
@@ -0,0 +1,26 @@
+import amqp from 'amqplib/callback_api'
+import logger from './utils/logger'
+
+
+const RABBITMQ_URL = process.env.RABBITMQ_URL!
+const POKEMON_ROUTING_KEY = process.env.POKEMON_ROUTING_KEY!
+
+
+amqp.connect(RABBITMQ_URL, (connError, connection) => {
+
+  if (connError) throw connError
+
+  connection.createChannel((error, channel) => {
+    if (error) throw error
+
+    channel.assertQueue(POKEMON_ROUTING_KEY, { durable: false })
+
+    logger.info(` [*] Waiting for messages in ${POKEMON_ROUTING_KEY}.`,)
+
+    channel.consume(POKEMON_ROUTING_KEY, (msg) => {
+      logger.info(` [x] Received ${msg?.content.toString()}`,)
+    }, { noAck: true })
+
+  })
+})
+
