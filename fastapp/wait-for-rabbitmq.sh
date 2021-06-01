39fb809eb5687b6a1afe2b21db7cf8126170a3ce
guilherme <guile.hm@hotmail.com>
2021-06-01 19:48:59 -0300

    increase sleep time

--- fastapp/wait-for-rabbitmq.sh
+++ fastapp/wait-for-rabbitmq.sh
@@ -3,7 +3,7 @@ set -e
 
 until timeout 1 bash -c "cat < /dev/null > /dev/tcp/spoon-rabbitmq/5672"; do
   >&2 echo "Rabbit MQ not up yet on spoon-rabbitmq"
-  sleep 1
+  sleep 3
 done
 
 echo "Rabbit MQ is up"
