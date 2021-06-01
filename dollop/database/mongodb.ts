cf6fd64e82874a501b79c6032c474f88f47bcb76
guilherme <guile.hm@hotmail.com>
2021-06-01 14:59:11 -0300

    create connection to mongo with mongoose

---
+++ dollop/database/mongodb.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import logger from '../utils/logger'
+
+
+const MONGODB_URI = process.env.MONGODB_URI!
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+    .then(() => logger.info('Connected to Mongo'))
+    .catch(e => logger.error('Error while trying to connect to Mongo', e))
+
+
+module.exports = mongoose
