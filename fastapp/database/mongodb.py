183af003c1197537353da880bdd58a4e6416ea0d
guilherme <guile.hm@hotmail.com>
2021-06-04 19:14:12 -0300

    fix db connection

--- fastapp/database/mongodb.py
+++ fastapp/database/mongodb.py
@@ -5,5 +5,5 @@ import motor.motor_asyncio
 MONGODB_URI = os.getenv('MONGODB_URI')
 
 DB_CLIENT = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
-DB_NAME = MONGODB_URI.rsplit(MONGODB_URI[-1])
+DB_NAME = MONGODB_URI.rsplit('/')[-1]
 db = DB_CLIENT[DB_NAME]
