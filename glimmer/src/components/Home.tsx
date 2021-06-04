a35a0e9c5629d9bf9919a7b678246a4d3cc9dff9
guilherme <guile.hm@hotmail.com>
2021-06-03 21:16:12 -0300

    create home component

---
+++ glimmer/src/components/Home.tsx
@@ -0,0 +1,24 @@
+import { PokemonDetailResult } from "../services/types"
+import PokemonCard from "./PokemonCard"
+
+
+interface PokemonDetailResultProps {
+  pokemonList: PokemonDetailResult[]
+}
+
+
+const Home: React.FC<PokemonDetailResultProps> = ({ pokemonList }) => {
+  return (
+    <div>
+      <main>
+        <h1>
+          Hello World!
+        </h1>
+        {pokemonList.map(pokemon => <PokemonCard key={pokemon.name} {...pokemon} />)}
+      </main>
+    </div>
+  )
+}
+
+
+export default Home
