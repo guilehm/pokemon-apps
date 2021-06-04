96120b3f847e1d0ae28ce89c8332b0b9f9d95f16
guilherme <guile.hm@hotmail.com>
2021-06-04 14:45:52 -0300

    refactor pokemon list component

--- glimmer/src/components/PokemonList.tsx
+++ glimmer/src/components/PokemonList.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import ApiService from '../services/api-service'
 import { PokemonDetailResult } from '../services/types'
 import PokemonCard from './PokemonCard'
 import * as S from './PokemonList.styles'
@@ -10,13 +12,43 @@ interface PokemonDetailResultProps {
 
 const PokemonList: React.FC<PokemonDetailResultProps> = ({ list }) => {
   const [pokemonList, setPokemonList] = useState<PokemonDetailResult[]>(list)
+  const [limit] = useState(pokemonList.length)
+  const [hasMore, setHasMore] = useState(true)
+
+
+  const Api = new ApiService()
+
+  const fetchNewResults = async () => {
+    const response = await Api.getPokemonList(
+      { offset: pokemonList.length, limit }
+    )
+    if (!response.data.length) {
+      setHasMore(false)
+      return
+    }
+    setPokemonList([...pokemonList, ...response.data])
+  }
+
   return (
-    <S.PokemonList>
-      {pokemonList.map(pokemon =>
-        <S.PokemonListItem key={pokemon.name}>
-          <PokemonCard {...pokemon} />
-        </S.PokemonListItem>)}
-    </S.PokemonList>
+
+    <InfiniteScroll
+      dataLength={pokemonList.length}
+      next={fetchNewResults}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      <S.PokemonList>
+        {pokemonList.map(pokemon =>
+          <S.PokemonListItem key={`${pokemon.name}`}>
+            <PokemonCard {...pokemon} />
+          </S.PokemonListItem>)}
+      </S.PokemonList>
+    </InfiniteScroll>
   )
 }
 
