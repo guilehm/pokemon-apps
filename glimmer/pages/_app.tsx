a82d61986861d3d9266f85f3fb54abf932fe65ea
guilherme <guile.hm@hotmail.com>
2021-06-04 13:01:12 -0300

    update globals css

--- glimmer/pages/_app.tsx
+++ glimmer/pages/_app.tsx
@@ -1,13 +1,13 @@
 import { AppProps } from 'next/app';
 import { createGlobalStyle } from 'styled-components'
-import '../styles/globals.css'
 
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  html, body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
   ul {
@@ -18,6 +18,16 @@ const GlobalStyle = createGlobalStyle`
   figure {
     margin: 0;
   }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
 `
 
 function MyApp({ Component, pageProps }: AppProps) {
