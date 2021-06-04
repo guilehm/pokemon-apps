c7a0cdabdb313c439169558e39957568364475bd
guilherme <guile.hm@hotmail.com>
2021-06-03 23:06:51 -0300

    create global style for app and create document

--- glimmer/pages/_app.tsx
+++ glimmer/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps, AppContext } from 'next/app';
+import { createGlobalStyle } from 'styled-components'
 import '../styles/globals.css'
 
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
