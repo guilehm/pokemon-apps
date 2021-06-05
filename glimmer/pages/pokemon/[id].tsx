69bb9ca0aec4534a5f67822bc012141bdefd8d19
guilherme <guile.hm@hotmail.com>
2021-06-05 10:09:46 -0300

    create static page for pokemon detail

---
+++ glimmer/pages/pokemon/[id].tsx
@@ -0,0 +1,39 @@
+import { GetStaticPaths } from 'next'
+import PokemonCard from '../../src/components/PokemonCard'
+import ApiService from '../../src/services/api-service'
+import { PokemonDetailResult, PokemonListResult } from '../../src/services/types'
+
+
+const Api = new ApiService()
+
+const PokemonDetailPage = (context: PokemonDetailResult) => {
+  return (
+    <PokemonCard {...context} />
+  )
+}
+
+export default PokemonDetailPage
+
+type Params = {
+  params: {
+    id: string
+  }
+}
+
+export const getStaticProps = async ({ params }: Params) => {
+  if (!params || !params.id) return
+  const response = await Api.getPokemonDetail(params.id)
+  return {
+    props: response.data,
+    revalidate: 60,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  const response = await Api.getPokemonList()
+  return {
+    paths: response.data.map((result: PokemonListResult) =>
+      ({ params: { id: result.id } })),
+    fallback: 'blocking'
+  }
+}
