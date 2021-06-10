6fc7e47014e6434e36bc73ca123dfdbcf4138a89
guilherme <guile.hm@hotmail.com>
2021-06-10 20:37:47 -0300

    create http server

--- goapp/main.go
+++ goapp/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
 func main() {
 	fmt.Println("Hello World")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
 }
