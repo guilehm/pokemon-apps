4938e5a04be096c992a8aa205216b4685aeb9963
guilherme <guile.hm@hotmail.com>
2021-06-10 20:44:10 -0300

    get port from environment variable

--- goapp/main.go
+++ goapp/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
+
 }
 
 func main() {
 	fmt.Println("Hello World")
 	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
