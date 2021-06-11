f7e66e6661d39633ad8bc7a5ce1ef5bf06bcb48f
guilherme <guile.hm@hotmail.com>
2021-06-10 21:56:17 -0300

    create connection to mongodb

--- goapp/main.go
+++ goapp/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -21,6 +27,25 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	fmt.Println("Hello World")
+
+	uri := os.Getenv("MONGODB_URI")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to Mongodb and pinged.")
 
 	http.HandleFunc("/goapp/hello", hello)
 	http.HandleFunc("/goapp/headers", headers)
