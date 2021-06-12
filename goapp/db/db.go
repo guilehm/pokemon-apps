4b334d7fed9a12e69216f440f79ea81160127a75
guilherme <guile.hm@hotmail.com>
2021-06-11 21:02:35 -0300

    create db module

---
+++ goapp/db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connection() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
