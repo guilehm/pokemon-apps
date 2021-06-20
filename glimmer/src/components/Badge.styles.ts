71b4fa6084bb8f579d9fa005ebfbcccf8dca3a3d
guilherme <guile.hm@hotmail.com>
2021-06-20 16:21:09 -0300

    create badge style

---
+++ glimmer/src/components/Badge.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+
+export const Badge = styled.span`
+  display: inline-block;
+  font-size: .75rem;
+  background-color: #44bd32;
+  padding: .35em .65em;
+  text-align: center;
+  border-radius: .25rem;
+  color: white;
+  white-space: nowrap;
+`;
