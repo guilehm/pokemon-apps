0c61f2d718ee8e6bb150122567a92b121da6938a
guilherme <guile.hm@hotmail.com>
2021-06-20 16:54:24 -0300

    css improvements

--- glimmer/src/components/Badge.styles.ts
+++ glimmer/src/components/Badge.styles.ts
@@ -4,10 +4,13 @@ import styled from 'styled-components'
 export const Badge = styled.span`
   display: inline-block;
   font-size: .75rem;
+  font-weight: 700;
   background-color: #44bd32;
   padding: .35em .65em;
   text-align: center;
   border-radius: .25rem;
   color: white;
   white-space: nowrap;
+  line-height: 1;
+  margin: 2px;
 `;
--- glimmer/src/components/PokemonCard.styles.ts
+++ glimmer/src/components/PokemonCard.styles.ts
@@ -3,10 +3,12 @@ import Image, { ImageProps } from 'next/image'
 
 
 export const Section = styled.section`
+  padding: 5px;
 `;
 
 export const Name = styled.h2`
   text-align: center;
+  margin: 0;
 `;
 
 export const Wrap = styled.div`
