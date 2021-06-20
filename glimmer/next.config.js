66aa6737b36bb2c1f8a4a4d05675bd34f73cfdaf
guilherme <guile.hm@hotmail.com>
2021-06-20 11:09:02 -0300

    set cache headers for images

--- glimmer/next.config.js
+++ glimmer/next.config.js
@@ -8,4 +8,17 @@ module.exports = {
       'raw.githubusercontent.com',
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/_next/image',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public,max-age=31536000,immutable',
+          },
+        ],
+      },
+    ]
+  },
 };
