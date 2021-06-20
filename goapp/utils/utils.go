4e8857a01423d8b0be2347c4ede093c7bc7e6e4b
guilherme <guile.hm@hotmail.com>
2021-06-20 09:23:50 -0300

    create utils package

---
+++ goapp/utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+)
+
+func LogRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
