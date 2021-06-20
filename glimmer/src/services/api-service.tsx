362b894ec30498941fe988348f12fcf1216ef4a8
guilherme <guile.hm@hotmail.com>
2021-06-20 09:40:05 -0300

    create environment variable to use python or go apis

--- glimmer/src/services/api-service.tsx
+++ glimmer/src/services/api-service.tsx
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
 import { PokemonDetailResult } from './types'
 
-const API_URL = process.env.API_URL || 'http://pokemon.local.com/fastapp'
+const API_URL = process.env.API_URL || 'http://pokemon.local.com/goapp'
+// const API_URL = process.env.API_URL || 'http://pokemon.local.com/fastapp'
 
 class ApiService {
   baseUrl: string
